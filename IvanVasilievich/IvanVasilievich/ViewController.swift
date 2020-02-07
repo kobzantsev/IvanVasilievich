9f820fe76235e98052fa3856c944ed42e9d11657
maxxx <kobzantsev.max@gmail.com>
2020-02-08 00:24:20 +0200

    BEST SOLUTION

--- IvanVasilievich/IvanVasilievich/ViewController.swift
+++ IvanVasilievich/IvanVasilievich/ViewController.swift
@@ -13,14 +13,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        SplityLine(string: "Vasyapupkin")
+        SplityLine(string: "VasyaPupkin")
         // Do any additional setup after loading the view.
     }
  
     func SplityLine(string: String){
         let regexp: NSRegularExpression = try! NSRegularExpression.init(pattern: "([a-z])([A-Z])", options: [])
-
-
         let range = NSMakeRange(0, string.count)
         let newString = regexp.stringByReplacingMatches(in: string, options: [], range: range, withTemplate: "$1 $2")
         let array: Array = newString.components(separatedBy: " ")
